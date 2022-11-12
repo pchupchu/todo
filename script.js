323d626932410c8b1df22ca8d92f8b86c99de042
Marina Malyarchuk <pchupchu@yandex.ru>
2022-11-12 19:10:55 +0300

    added drag and drop

--- script.js
+++ script.js
@@ -64,3 +64,37 @@ form.addEventListener('submit', handleSubmitAddTodoForm);
 todoList.forEach((dataCard) => {
   renderCard(dataCard);
 })
+
+
+
+// Перетаскивание
+const cards = todoContainer.querySelectorAll('.todo-card');
+
+todoContainer.addEventListener('dragstart', (event) => {
+  event.target.classList.add('selected');
+})
+
+todoContainer.addEventListener('dragend', (event) => {
+  event.target.classList.remove('selected');
+})
+
+todoContainer.addEventListener('dragover', (event) => {
+  event.preventDefault();
+
+  const activeElement = todoContainer.querySelector('.selected');
+  const currentElement = event.target;
+  const isMoveable = activeElement !== currentElement &&
+    currentElement.classList.contains('todo-card');
+
+  if (!isMoveable) {
+    return;
+  }
+
+  // Находим элемент, перед которым будем вставлять
+  const nextElement = (currentElement === activeElement.nextElementSibling) ?
+      currentElement.nextElementSibling :
+      currentElement;
+
+  // Вставляем activeElement перед nextElement
+  todoContainer.insertBefore(activeElement, nextElement);
+})
